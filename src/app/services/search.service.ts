6614e7682770f8a93b947196e4fea9705d97baeb
CARLOS\Carlo <coli.okis@gmail.com>
2025-02-10 12:02:50 +0100

    ltile change

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -2,27 +2,22 @@ import {inject, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable, Subject, switchMap} from "rxjs";
 import {Cosmetico} from "../common/interfaceApi";
+import {environment} from "../environment/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
 
-  private readonly urlBase:string = "https://api-cosmeticos.vercel.app/api/v2/cosmeticos";
+  private readonly urlBase:string = environment.urlBase;
   private readonly http: HttpClient = inject(HttpClient);
+
   private palabra: Subject<string> = new Subject<string>();
   private productSearched$: Observable<Cosmetico[]> = this.palabra.pipe(
     switchMap(res => {
         return this.http.get<Cosmetico[]>(this.urlBase+'/cosmeticoByName/'+res);
       })
   );
-
-  /**
-   *  Monataremos la peticion mediante un Switchmap
-   *  Necesitamos un subject que a la vez sea observer y observable
-   *  Tendremos la palabra que irá cambiando y tendremos que estar suscritos
-   *  a esa palabra para que en funcion de la misma se hagan las peticiones a la API
-   */
 
   constructor() {}
 
@@ -34,5 +29,12 @@ export class SearchService {
     start():Observable<Cosmetico[]> {
       return this.productSearched$;
     }
+
+  /**
+   *  Monataremos la peticion mediante un Switchmap
+   *  Necesitamos un subject que a la vez sea observer y observable
+   *  Tendremos la palabra que irá cambiando y tendremos que estar suscritos
+   *  a esa palabra para que en funcion de la misma se hagan las peticiones a la API
+   */
 
 }
